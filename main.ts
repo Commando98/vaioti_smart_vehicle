8375fe17fb9a1ab4614bb09799fc420764bb1dae
Commando98 <mukularora649@gmail.com>
2017-07-17 12:25:29 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import {SearchPage} from "../search/search";
+
+
+/**
+ * Generated class for the MainPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-main',
+  templateUrl: 'main.html',
+})
+export class MainPage {
+  bus_number:string;
+  stoppage:any=["Hamirpur","Bhota","Mehre","Badsar","Bangana","Una (Your Destination)"];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl:AlertController) {
+    this.bus_number=this.navParams.get("bus_number");
+  }
+  confirm() {
+    this.navCtrl.pop();
+  }
+  search(){
+    this.navCtrl.push(SearchPage);
+  }
+  ionViewDidLoad(){
+    console.log('ionViewDidLoad MainPage');
+  }
+
+}
